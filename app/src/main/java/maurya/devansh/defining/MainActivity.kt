272ecb1044e6fceaf77c48ae4dda4673679804aa
Devansh-Maurya <devansh233@gmail.com>
2022-06-29 20:02:20 +0530

    Show touch coordinates

--- app/src/main/java/maurya/devansh/defining/MainActivity.kt
+++ app/src/main/java/maurya/devansh/defining/MainActivity.kt
@@ -1,11 +1,29 @@
 package maurya.devansh.defining
 
+import android.graphics.PointF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.TextView
+import maurya.devansh.defining.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        binding.tvExactLocation.setTouchLocationText("Exact", event?.getTouchPoint())
+        return super.onTouchEvent(event)
+    }
+
+    private fun TextView.setTouchLocationText(title: String, point: PointF?) {
+        text = "$title location\n(${point?.x}, ${point?.y})"
+    }
+
+    private fun MotionEvent.getTouchPoint(): PointF = PointF(rawX, rawY)
 }
